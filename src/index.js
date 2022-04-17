21ba1d5b10d3d2ce3127ce844b76b8cdd55b3806
BRB3D <samuraix221@hotmail.com>
2022-04-16 22:40:45 -0400

    contructor method for class component

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 
 
 class App extends React.Component {
+  // constructor function is particular of javascript lanaguage and not react.
+  constructor(props) {
+    super(props);//reference to the parent constructor function.
+    this.state = { lat: null };//lat is abbreviated from latitude
+  }
+
+  // React need to define render !!
   render() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => console.log(position), (err) => console.log(err)
     );
-    return <div>Lattitude: </div>
+    return <div>Latitude: {position.coords.latitude}</div>
   }
 }
 
