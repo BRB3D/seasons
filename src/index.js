9c04929b13c5d163db2e46cd99add80cbdd9b882
BRB3D <samuraix221@hotmail.com>
2022-04-16 23:07:35 -0400

    conditional rendering class component

--- src/index.js
+++ src/index.js
@@ -6,19 +6,25 @@ class App extends React.Component {
   // constructor function is particular of javascript lanaguage and not react.
   constructor(props) {
     super(props);//reference to the parent constructor function.
-    this.state = { lat: null };//lat is abbreviated from latitude
+    this.state = { lat: null, errorMessage: '' };//lat is abbreviated from latitude
 
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         this.setState({ lat: position.coords.latitude })
-      }, (err) => console.log(err)
+      },
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
 
   // React need to define render !!
   render() {
-
-    return <div>Latitude: {this.state.lat}</div>
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage} </div>
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>
+    }
+    return <div>Loading!</div>
   }
 }
 
