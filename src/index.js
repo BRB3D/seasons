263ca0d38720bc2b16f5ff9ceebb022a6636417f
BRB3D <samuraix221@hotmail.com>
2022-04-16 22:25:57 -0400

    functional compontent

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ const App = () => {
   window.navigator.geolocation.getCurrentPosition(
     (position) => console.log(position), (err) => console.log(err)
   );
-  return <div>Hi there!</div>
+  return (
+    <div>Lattitude: </div>
+  );
 };
 
 ReactDOM.render(
