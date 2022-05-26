741760e8b8f9bdd2f2fdaacc6bda43719ab8f69d
BRB3D <samuraix221@hotmail.com>
2022-05-26 16:10:41 -0400

    pass props to children

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from 'react-dom';
+import SeasonDisplay from "./SeasonDisplay";
 
 
 class App extends React.Component {
@@ -13,9 +14,7 @@ class App extends React.Component {
 
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude })
-      },
+      (position) => this.setState({ lat: position.coords.latitude }),
       (err) => this.setState({ errorMessage: err.message })
     );
   }
@@ -27,7 +26,7 @@ class App extends React.Component {
       return <div>Error: {this.state.errorMessage} </div>
     }
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>
+      return <SeasonDisplay lat={this.state.lat}/>
     }
     return <div>Loading!</div>
   }
